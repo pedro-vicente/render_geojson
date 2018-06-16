a336aaecce2b500e229fc7eb31a89c36dafbd31f
pvn <pedro.vicente@space-research.org>
2018-06-16 14:24:15 -0400

    read geojson

--- render_geojson.cc
+++ render_geojson.cc
@@ -1,107 +1,229 @@
+#include "render_geojson.hh"
 
-#include "wx/wxprec.h"
-#include "wx/wx.h"
-#include "sample.xpm"
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//wxAppMinimal
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class wxAppMinimal : public wxApp
-{
-public:
-  virtual bool OnInit() wxOVERRIDE;
-};
-
-wxIMPLEMENT_APP(wxAppMinimal);
-
+const int panel_size = 510;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//wxFrameMinimal
+//wxAppAlert::OnInit()
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class wxFrameMinimal : public wxFrame
-{
-public:
-  wxFrameMinimal(const wxString& title);
-  void OnQuit(wxCommandEvent& event);
-  void OnAbout(wxCommandEvent& event);
-private:
-  wxDECLARE_EVENT_TABLE();
-};
+wxIMPLEMENT_APP(wxAppAlert);
 
-enum
-{
-  Minimal_Quit = wxID_EXIT,
-  Minimal_About = wxID_ABOUT
-};
-
-wxBEGIN_EVENT_TABLE(wxFrameMinimal, wxFrame)
-EVT_MENU(Minimal_Quit, wxFrameMinimal::OnQuit)
-EVT_MENU(Minimal_About, wxFrameMinimal::OnAbout)
-wxEND_EVENT_TABLE()
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//wxAppMinimal::OnInit()
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-bool wxAppMinimal::OnInit()
+bool wxAppAlert::OnInit()
 {
   if (!wxApp::OnInit())
   {
     return false;
   }
-  wxFrameMinimal *frame = new wxFrameMinimal("Minimal");
+  wxFrameMain *frame = new wxFrameMain();
   frame->Show(true);
   frame->Maximize();
   return true;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//wxFrameMinimal::wxFrameMinimal
+//wxFrameMain::wxFrameMain
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-wxFrameMinimal::wxFrameMinimal(const wxString& title)
-  : wxFrame(NULL, wxID_ANY, title)
+wxBEGIN_EVENT_TABLE(wxFrameMain, wxFrame)
+EVT_MENU_RANGE(wxID_FILE1, wxID_FILE9, wxFrameMain::OnMRUFile)
+EVT_MENU(wxID_OPEN, wxFrameMain::OnFileOpen)
+EVT_MENU(wxID_EXIT, wxFrameMain::OnQuit)
+EVT_MENU(wxID_ABOUT, wxFrameMain::OnAbout)
+wxEND_EVENT_TABLE()
+
+wxFrameMain::wxFrameMain()
+  : wxFrame(NULL, wxID_ANY, "GeoJSON reader")
 {
   SetIcon(wxICON(sample));
   wxMenu *menu_file = new wxMenu;
-  menu_file->Append(Minimal_Quit, "E&xit\tAlt-X", "Quit this program");
+  menu_file->Append(wxID_OPEN, "O&pen\tAlt-O", "Open file");
+  menu_file->Append(wxID_EXIT, "E&xit\tAlt-X", "Quit this program");
   wxMenu *menu_help = new wxMenu;
-  menu_help->Append(Minimal_About, "&About\tF1", "Show about dialog");
+  menu_help->Append(wxID_ABOUT, "&About\tF1", "Show about dialog");
   wxMenuBar *menu_bar = new wxMenuBar();
   menu_bar->Append(menu_file, "&File");
   menu_bar->Append(menu_help, "&Help");
   SetMenuBar(menu_bar);
   CreateStatusBar(2);
-  SetStatusText("Ready");
 
-  wxPanel *win_input = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize,
-    wxDOUBLE_BORDER);
-  win_input->SetMinSize(wxSize(600, -1));
+  m_splitter = new wxSplitterWindow(this);
+  m_splitter->SetSashInvisible(true);
+  wxPanel *grid = new wxPanel(m_splitter, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxDOUBLE_BORDER);
+  grid->SetMinSize(wxSize(panel_size, -1));
+  wxWindow *chart = new wxWindow(m_splitter, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxDOUBLE_BORDER);
+  m_splitter->SplitVertically(grid, chart, panel_size);
 
-  wxWindow *win_grid = new wxWindow(this, wxID_ANY);
-  win_grid->SetMinSize(wxSize(200, -1));
+  //avoid update on empty windows 
+  m_win_grid = NULL;
+  m_win_chart = NULL;
 
-  wxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-  sizer->Add(win_input, 0, wxGROW);
-  sizer->Add(win_grid, 1, wxGROW);
-  SetSizerAndFit(sizer);
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+  //file history
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  m_file_history.UseMenu(menu_file);
+  m_file_history.AddFilesToMenu(menu_file);
+  m_file_history.Load(*wxConfig::Get());
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//wxFrameMinimal::OnQuit
+//wxFrameMain::~wxFrameMain
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void wxFrameMinimal::OnQuit(wxCommandEvent& WXUNUSED(event))
+wxFrameMain::~wxFrameMain()
 {
+  m_file_history.Save(*wxConfig::Get());
+  if (m_win_chart)
+  {
+    m_win_chart->Destroy();
+  }
+  if (m_win_grid)
+  {
+    m_win_grid->Destroy();
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxFrameMain::OnQuit
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void wxFrameMain::OnQuit(wxCommandEvent& WXUNUSED(event))
+{
+  m_file_history.Save(*wxConfig::Get());
   Close(true);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//wxFrameMinimal::OnAbout
+//wxFrameMain::OnAbout
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void wxFrameMinimal::OnAbout(wxCommandEvent& WXUNUSED(event))
+void wxFrameMain::OnAbout(wxCommandEvent& WXUNUSED(event))
 {
+  wxString str("Pedro Vicente (2018)\n\n");
+  wxMessageBox(str, "GeoJSON reader", wxOK, this);
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxFrameMain::OnMRUFile
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void wxFrameMain::OnMRUFile(wxCommandEvent& event)
+{
+  size_t idx = event.GetId() - wxID_FILE1;
+  wxString path(m_file_history.GetHistoryFile(idx));
+  if (!path.empty())
+  {
+    if (read(path.ToStdString()) < 0)
+    {
+      m_file_history.RemoveFileFromHistory(idx);
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxFrameMain::OnFileOpen
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void wxFrameMain::OnFileOpen(wxCommandEvent &WXUNUSED(event))
+{
+  wxString path;
+  wxFileDialog dlg(this, wxT("Open file"),
+    wxEmptyString,
+    wxEmptyString,
+    wxString::Format
+    (
+      wxT("geojson (*.geojson)|*.geojson|All files (%s)|%s"),
+      wxFileSelectorDefaultWildcardStr,
+      wxFileSelectorDefaultWildcardStr
+    ),
+    wxFD_OPEN | wxFD_FILE_MUST_EXIST | wxFD_CHANGE_DIR);
+  if (dlg.ShowModal() != wxID_OK)
+  {
+    return;
+  }
+  path = dlg.GetPath();
+  if (this->read(path.ToStdString()) != 0)
+  {
+    return;
+  }
+  m_file_history.AddFileToHistory(path);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxFrameMain::read
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int wxFrameMain::read(const std::string &file_name)
+{
+  m_win_chart = m_splitter->GetWindow2();
+  wxChart *chart = new wxChart(m_splitter);
+  if (chart->read_file(file_name) < 0)
+  {
+    chart->Destroy();
+    return -1;
+  }
+
+  m_splitter->ReplaceWindow(m_win_chart, chart);
+  m_win_chart->Destroy();
+  m_win_chart = chart;
+
+  m_current_file = file_name;
+  SetTitle(file_name);
+  return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxChart::wxChart()
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+wxBEGIN_EVENT_TABLE(wxChart, wxScrolledWindow)
+EVT_LEFT_DOWN(wxChart::OnMouseDown)
+EVT_MOTION(wxChart::OnMouseMove)
+wxEND_EVENT_TABLE()
+
+wxChart::wxChart(wxWindow *parent) :
+  wxScrolledWindow(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize,
+    wxDOUBLE_BORDER)
+{
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxChart::read_file
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int wxChart::read_file(const std::string &file_name)
+{
+
+  return 0;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxChart::OnDraw
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void wxChart::OnDraw(wxDC& dc)
+{
+  dc.DrawRectangle(0, 0, 200, 200);
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxChart::OnMouseDown
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void wxChart::OnMouseDown(wxMouseEvent &event)
+{
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxChart::OnMouseMove
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void wxChart::OnMouseMove(wxMouseEvent &event)
+{
+
+}
+
---
+++ render_geojson.hh
@@ -0,0 +1,81 @@
+#ifndef WX_RENDER_GEOJSON
+#define WX_RENDER_GEOJSON 1
+
+#include "wx/wxprec.h"
+#include "wx/wx.h"
+#include "wx/splitter.h"
+#include "wx/artprov.h"
+#include "wx/imaglist.h"
+#include "wx/grid.h"
+#include "wx/mdi.h"
+#include "wx/toolbar.h"
+#include "wx/laywin.h"
+#include "wx/list.h"
+#include "wx/cmdline.h"
+#include "wx/datetime.h"
+#include "wx/datectrl.h"
+#include "wx/stattext.h"
+#include "wx/dateevt.h"
+#include "wx/panel.h"
+#include "wx/calctrl.h"
+#include "wx/timectrl.h"
+#include "wx/collpane.h"
+#include "sample.xpm"
+#include "grafix.hh"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxChart
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class wxChart : public wxScrolledWindow
+{
+public:
+  wxChart(wxWindow *parent);
+  virtual void OnDraw(wxDC& dc);
+  void OnMouseDown(wxMouseEvent &event);
+  void OnMouseMove(wxMouseEvent &event);
+  int read_file(const std::string &file_name);
+  graf_t m_graf;
+
+private:
+  wxDECLARE_EVENT_TABLE();
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxFrameMain
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class wxFrameMain : public wxFrame
+{
+public:
+  wxFrameMain();
+  ~wxFrameMain();
+  void OnFileOpen(wxCommandEvent &event);
+  void OnMRUFile(wxCommandEvent& event);
+  void OnQuit(wxCommandEvent& event);
+  void OnAbout(wxCommandEvent& event);
+  wxString m_current_file;
+
+protected:
+  wxWindow *m_win_grid;
+  wxWindow *m_win_chart;
+  wxSplitterWindow* m_splitter;
+  int read(const std::string &file_name);
+  wxFileHistory m_file_history;
+
+private:
+  wxDECLARE_EVENT_TABLE();
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//wxAppAlert
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class wxAppAlert : public wxApp
+{
+public:
+  virtual bool OnInit();
+};
+
+#endif
+
